afa3c32a0f4402285d43e0e5ce6ff0d1b5773365
lizhisuao <793544027@qq.com>
2019-07-11 15:32:18 +0800

    项目整体初始化

---
+++ src/main/java/com/blog/mapper/ArticleMapper.java
@@ -0,0 +1,30 @@
+package com.blog.mapper;
+
+import com.blog.model.Article;
+import com.blog.model.ArticleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ArticleMapper {
+    int countByExample(ArticleExample example);
+
+    int deleteByExample(ArticleExample example);
+
+    int deleteByPrimaryKey(Integer tId);
+
+    int insert(Article record);
+
+    int insertSelective(Article record);
+
+    List<Article> selectByExample(ArticleExample example);
+
+    Article selectByPrimaryKey(Integer tId);
+
+    int updateByExampleSelective(@Param("record") Article record, @Param("example") ArticleExample example);
+
+    int updateByExample(@Param("record") Article record, @Param("example") ArticleExample example);
+
+    int updateByPrimaryKeySelective(Article record);
+
+    int updateByPrimaryKey(Article record);
+}
---
+++ src/main/java/com/blog/model/Article.java
@@ -0,0 +1,55 @@
+package com.blog.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+
+public class Article implements Serializable {
+    @ApiModelProperty(value = "文章的id")
+    private Integer tId;
+
+    @ApiModelProperty(value = "文章标题")
+    private String title;
+
+    @ApiModelProperty(value = "文章内容")
+    private String content;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer gettId() {
+        return tId;
+    }
+
+    public void settId(Integer tId) {
+        this.tId = tId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", tId=").append(tId);
+        sb.append(", title=").append(title);
+        sb.append(", content=").append(content);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
